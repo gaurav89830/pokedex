d8dc1560f0886aefe64b2466d5ddc126a94d8a17
Gaurav Sharma <gaurav89830@gmail.com>
2023-07-30 20:16:20 +0530

    catch error update

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 document.querySelector("#search").addEventListener("click", getPokemon);
 
 function toLowerCase(string) {
-    return string.toLowerCase(name);
+  return string.toLowerCase(name);
 }
 
 function getPokemon(e) {
-    var name = document.querySelector("#pokemon-name").value;
-    name = toLowerCase(name);
-    name.toLowerCase();
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then((response) => response.json())
-        .then((data) => {
-            // document.querySelector(".img-svg").src = data.sprites.other["official-artwork"].front_default;
-            document.querySelector(".img-gif").src = data.sprites.versions["generation-v"]["black-white"].animated.front_default;
-            document.querySelector(".pokemon-name").innerHTML = data.name;
-            document.querySelector(".pokemon-weight").innerHTML = "Weight : " + data.weight;
-            document.querySelector(".pokemon-height").innerHTML = "Height : " + data.height;
-            const abilityNames = data.abilities.map(obj => obj.ability.name);
-            const pTag = document.querySelector('.pokemon-abilities');
-            pTag.textContent = abilityNames.join('  ');
-        })
-        .catch((err) => {
-            console.log("Pokemon nhi mila", err);
-        });
+  var name = document.querySelector("#pokemon-name").value;
+  name = toLowerCase(name);
+  name.toLowerCase();
+  fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.querySelector(".secondary-div").classList.add("hidden");
+      document.querySelector(".secondary-div").classList.remove("visible");
+      document.querySelector(".main-div").classList.remove("hidden");
+      // document.querySelector(".img-svg").src = data.sprites.other["official-artwork"].front_default;
+      document.querySelector(".img-gif").src =
+        data.sprites.versions["generation-v"][
+          "black-white"
+        ].animated.front_default;
+      document.querySelector(".pokemon-name").innerHTML = data.name;
+      document.querySelector(".pokemon-weight").innerHTML =
+        "Weight : " + data.weight;
+      document.querySelector(".pokemon-height").innerHTML =
+        "Height : " + data.height;
+      const abilityNames = data.abilities.map((obj) => obj.ability.name);
+      const pTag = document.querySelector(".pokemon-abilities");
+      pTag.textContent = abilityNames.join("  ");
+    })
+    .catch((err) => {
+      console.log("Pokemon nhi mila", err);
+      document.querySelector(".secondary-div").classList.remove("hidden");
+      document.querySelector(".secondary-div").classList.add("visible");
+    });
 }
 
 const searchInput = document.querySelector("#pokemon-name");
@@ -30,7 +40,7 @@ searchInput.focus();
 
 // Search when "Enter" key is pressed
 searchInput.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        getPokemon();
-    }
+  if (e.key === "Enter") {
+    getPokemon();
+  }
 });
